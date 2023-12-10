aad3f63ea962a7023357b7a34c409222d80d8272
Akramul Hasan <lighthouse8048@gmail.com>
2023-12-10 15:16:33 +0600

    One settings section added

--- admin/class.admin-settings-pages.php
+++ admin/class.admin-settings-pages.php
@@ -13,8 +13,38 @@ if(!class_exists('wpfyAdminSettingsPages')){
 
 
         public function admin_init(){
+
+            register_setting( 'wpfy_pi_group', 'wpfy_pi_options' );
+
+            add_settings_section( 
+                'wpfy_pi_main_section', 
+                'How does it work?', 
+                null, 
+                'wpfy_pi_page1' 
+            );
+
+            add_settings_field( 
+                'wpfy_pi_shortcode', 
+                'Shortcode', 
+                array($this, 'shortcode_callback'),
+                'wpfy_pi_page1', 
+                'wpfy_pi_main_section'
+            );
             
         }
+
+
+        /**
+         * 
+         * Shortcode Field callback description
+         * 
+         */
+
+         public function shortcode_callback(){
+            ?>
+                <span>Here will have some descirption of the field</span>
+            <?php
+         }
     }
 
 }
--- admin/settings-page.php
+++ admin/settings-page.php
@@ -2,6 +2,10 @@
     <h1><?php echo esc_html( get_admin_page_title( ) ); ?></h1>
 
     <form action="options.php" method="post">
-
+        <?php 
+            settings_fields( 'wpfy_pi_group' );
+            do_settings_sections( 'wpfy_pi_page1' );
+            submit_button( 'Save Settings');
+        ?>
     </form>
 </div>
