ed1711f2e71b1227adad43b3ebb0aa9f7de5e6b9
Akramul Hasan <lighthouse8048@gmail.com>
2023-12-15 15:34:18 +0600

    Form submission connected with custom endpoint

--- assets/js/inquiry-popup.js
+++ assets/js/inquiry-popup.js
@@ -5,7 +5,6 @@ function closeInquiryPopup() {
 
 function resetFormFields() {
     jQuery('#wpfy-inquiry-form')[0].reset();
-
 }
 
 jQuery(document).ready(function ($) {
@@ -15,7 +14,28 @@ jQuery(document).ready(function ($) {
 
     $('#wpfy-inquiry-form').on('submit', function (e) {
         e.preventDefault();
-        alert('Inquiry submitted!');
-        closeInquiryPopup(); // Close the popup after submission
+
+        // Gather form data
+        var formData = {
+            'name': $('#wpfy-inquiry-name').val(),
+            'email': $('#wpfy-inquiry-email').val(),
+            'message': $('#wpfy-inquiry-message').val()
+        };
+
+        // Submit form data to the REST API
+        $.ajax({
+            url: '/wp-json/wpfypi/v1/submit-inquiry', // Your custom endpoint
+            method: 'POST',
+            data: JSON.stringify(formData),
+            contentType: 'application/json',
+            success: function(response) {
+                alert('Inquiry submitted successfully!');
+                closeInquiryPopup(); // Close the popup after submission
+            },
+            error: function(response) {
+                console.error(response);
+                alert('There was a problem submitting your inquiry. Please try again.');
+            }
+        });
     });
 });
